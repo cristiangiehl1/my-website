18de57a94ef9366dd22b2bc71225057c7de46d5c
cristiangiehl1 <cristiangiehl@gmail.com>
2026-01-11 17:23:19 -0300

    feat: add `.prettierignore` and `prettier.config.mjs`

---
+++ prettier.config.mjs
@@ -0,0 +1,24 @@
+/** @typedef {import('prettier').Config} PrettierConfig */
+
+/** @type { PrettierConfig } */
+
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  bracketSameLine: false,
+  arrowParens: 'always',
+  proseWrap: 'preserve', // 'always', 'never'
+}
+
+export default config
