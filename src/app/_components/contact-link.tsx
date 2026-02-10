d2af05447120e5456097c51c655a7e78f54084bb
cristiangiehl1 <cristiangiehl@gmail.com>
2026-02-09 21:40:56 -0300

    feat: add `ContactLink` component

---
+++ src/app/_components/contact-link.tsx
@@ -0,0 +1,35 @@
+import type { HTMLAttributes } from 'react'
+import type { IconType } from 'react-icons'
+
+import { cn } from '@/lib/utils'
+
+interface ContactLinkProps extends HTMLAttributes<HTMLAnchorElement> {
+  Icon: IconType
+  href: string
+  ariaLabel: string
+}
+
+export function ContactLink({
+  Icon,
+  href,
+  ariaLabel,
+  className,
+  ...props
+}: ContactLinkProps) {
+  return (
+    <a
+      href={href}
+      aria-label={`Navegar para ${ariaLabel}`}
+      target='_blank'
+      rel='noopener noreferrer'
+      className={cn(
+        'bg-muted flex h-12 w-12 transform items-center justify-center rounded-lg',
+        'transition-all duration-300',
+        'hover:bg-primary hover:text-primary-foreground hover:scale-110',
+        className
+      )}
+      {...props}>
+      <Icon size={20} />
+    </a>
+  )
+}
